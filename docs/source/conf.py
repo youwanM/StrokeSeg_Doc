e1feb94da128049cf11f9f16fdfdbd3da644adeb
Youwan <youwan.mahe@gmail.com>
2025-08-22 21:29:19 +0200

    Update project information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'StrokeSeg'
+copyright = '2025, Empenn Research Team'
+author = 'Empenn Research Team'
 
 release = '0.1'
 version = '0.1.0'
